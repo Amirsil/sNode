fe6bcc25e32c13ee43fdcca1965b39a15b4f4424
unknown <amirsil2002@gmail.com>
2021-09-01 23:31:27 +0300

    Fix support for custom allocator

--- include/s_node.h
+++ include/s_node.h
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 typedef struct node_t{
 		const char *str;
 		struct node_t *next;
-
-	}node_t;
+	} node_t;
 
 void FreeList(node_t **ptr);
 void RemoveNode(node_t **ptr, const char *str);
 void PrintNodes(node_t **ptr);
 void CreateNode(node_t **ptr, const char *str); 
 
-void *(*malloc_fn)(size_t size);
-void (*free_fn)(void *ptr);
 /*
 The allocator is defaulted to glibc's general purpose allocator, to change it to your own custom allocator simply change malloc_fc and free_fn
 */	
+#ifndef malloc_fn
+#define malloc_fn(X) malloc(X)
+#endif
+
+#ifndef free_fn
+#define free_fn(X) free(X)
+#endif
--- src/s_node.c
+++ src/s_node.c
@@ -1,20 +1,17 @@
 #include "s_node.h"
 
-
-/*
-The allocator is defaulted to glibc's general purpose allocator, to change it to your own custom allocator simply change malloc_fc and free_fn
-*/
-
-void *(*malloc_fn)(size_t size) = &malloc;
-void (*free_fn)(void *ptr) = &free;
-
-
 void CreateNode(node_t **ptr, const char *str)
 {
-	node_t *newptr;
-	if (!( newptr = (node_t *)malloc_fn(sizeof(node_t)))) printf("Failed to malloc_fn memory in the heap :(");
-	if (*ptr) *newptr = (node_t){str, *ptr};
-	else *newptr = (node_t){str, NULL};
+	node_t *newptr = (node_t *)malloc_fn(sizeof(node_t));
+	if (newptr == NULL) {
+		printf("Failed to malloc_fn memory in the heap :(");
+		return;
+	}
+
+	*newptr = *ptr \
+				? (node_t){str, *ptr} \
+				: (node_t){str, NULL};
+	
 	printf("Added \"%s\" (%p)\n", newptr -> str, newptr);
 	*ptr = newptr;
 }
@@ -59,7 +56,6 @@ void PrintNodes(node_t **ptr)
 	printf("[");
 	for (node_t *node = *ptr; node ; node = node -> next) printf("\"%s\" -> ", node -> str);
 	printf("NULL]\n");
-	return;
 }
 
 void FreeList(node_t **ptr)
